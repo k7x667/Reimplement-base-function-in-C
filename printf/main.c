f115f02b46a0a725288877749cbc585c5df76507
K7 <k7.667.k7@gmail.com>
2024-01-07 19:12:48 +0100

    clean code

--- printf/main.c
+++ printf/main.c
@@ -1,37 +1,72 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void newPrintf(const char *format, ...) {
+
+void newPrintf(const char *format, ...) 
+{
     va_list args;
     va_start(args, format);
 
-    while (*format != '\0') {
-        if (*format == '%') {
+
+    while (*format != '\0') 
+    {
+
+        if (*format == '%') 
+        {
+
             format++;
-            switch (*format) {
+
+
+            switch (*format) 
+            {
+
                 case 'd':
                     printf("%d", va_arg(args, int));
+
                     break;
+
+
                 case 's':
                     printf("%s", va_arg(args, char *));
+
                     break;
+
+
                 case 'c':
                     printf("%c", va_arg(args, int)); 
+
                     break;
+
+
                 default:
+
                     putchar('%');
                     putchar(*format);
             }
-        } else {
+
+        } 
+            else 
+        {
+
             putchar(*format);
+
         }
+
         format++;
     }
 
     va_end(args);
+
 }
 
-int main() {
-    newPrintf("Hello, %s! The answer is %d. Character: %c\n", "world", 42, 'A');
+int main() 
+{
+    newPrintf(
+        "Hello, %s! The answer is %d. Character: %c\n", 
+        "world", 
+        42, 
+        'A'
+    );
+
     return 0;
 }
