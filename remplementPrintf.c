3f7f1e4e231d771636997eb3401897aba5e2404e
K7 <k7.667.k7@gmail.com>
2024-01-07 18:56:00 +0100

    newPrintf()

--- remplementPrintf.c
+++ remplementPrintf.c
@@ -1,13 +1,13 @@
 #include <unistd.h> // write import function
 
 void newPrintf(const char *str) {
-    while (*str) {
+    while (str) {
         write(1, str, 1);
         str++;
     }
 }
 
 int main() {
-    newPrintf('Hello world! \n');
+    newPrintf("Hello world!");
     return 0;
 }
