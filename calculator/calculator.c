fdacba977a000f8158d903d374a2ce75b24e6dff
K7 <k7.667.k7@gmail.com>
2024-01-07 23:00:47 +0100

    documentation

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -5,29 +5,33 @@ int calculator()
     char operator;
     double nbrA, nbrB;
 
+    // input for nbrA
     printf("Enter an operator (+, /, -, *) ");
     scanf("%c", &operator);
 
+    // input for nbrB
     printf("Enter two operands ;");
     scanf("%lf %lf", &nbrA, &nbrB);
 
+    // Switch work like if/else/elseif
     switch (operator)
     {
-    case '+':
-      printf("%.1lf + %.1lf = %.1lf", nbrA, nbrB, nbrA + nbrB);
-      break;
-    case '-':
-      printf("%.1lf - %.1lf = %.1lf", nbrA, nbrB, nbrA - nbrB);
-      break;
-    case '*':
-      printf("%.1lf * %.1lf = %.1lf", nbrA, nbrB, nbrA * nbrB);
-      break;
-    case '/':
-      printf("%.1lf / %.1lf = %.1lf", nbrA, nbrB, nbrA / nbrB);
-      break;
-    
-    default:
-        printf("operator incorrect");
+        case '+':
+            printf("%.1lf + %.1lf = %.1lf", nbrA, nbrB, nbrA + nbrB);
+            break;
+        case '-':
+            printf("%.1lf - %.1lf = %.1lf", nbrA, nbrB, nbrA - nbrB);
+            break;
+        case '*':
+            printf("%.1lf * %.1lf = %.1lf", nbrA, nbrB, nbrA * nbrB);
+            break;
+        case '/':
+            printf("%.1lf / %.1lf = %.1lf", nbrA, nbrB, nbrA / nbrB);
+            break;
+        
+        default:
+            printf("operator incorrect.");
+            break;
     }
 
     return 0;
