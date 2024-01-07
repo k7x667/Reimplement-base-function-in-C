84bb229f4e97deb345cb3d539604b742766a74aa
K7 <k7.667.k7@gmail.com>
2024-01-07 18:50:49 +0100

    printf function reimplementation

---
+++ remplementPrintf.c
@@ -0,0 +1,13 @@
+#include <unistd.h> // write import function
+
+void newPrintf(const char *str) {
+    while (*str) {
+        write(1, str, 1);
+        str++;
+    }
+}
+
+int main() {
+    newPrintf('Hello world! \n');
+    return 0;
+}
