c77affa30e3dcc6e44868f3cdc594c30447b5896
K7 <k7.667.k7@gmail.com>
2024-01-07 19:05:39 +0100

    add printf()

--- reimplementPrintf.c
+++ reimplementPrintf.c
@@ -1,13 +1,37 @@
-#include <unistd.h> // write import function
+#include <stdio.h>
+#include <stdarg.h>
 
-void newPrintf(const char *str) {
-    while (str) {
-        write(1, str, 1);
-        str++;
+void newPrintf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++; // Passer le '%'
+            switch (*format) {
+                case 'd':
+                    printf("%d", va_arg(args, int));
+                    break;
+                case 's':
+                    printf("%s", va_arg(args, char *));
+                    break;
+                case 'c':
+                    printf("%c", va_arg(args, int)); // 'char' est promu en 'int' dans va_arg
+                    break;
+                default:
+                    putchar('%'); // Afficher le caractère '%' lui-même s'il n'est suivi d'aucun format valide
+                    putchar(*format);
+            }
+        } else {
+            putchar(*format);
+        }
+        format++;
     }
+
+    va_end(args);
 }
 
 int main() {
-    newPrintf("Hello world!");
+    newPrintf("Hello, %s! The answer is %d. Character: %c\n", "world", 42, 'A');
     return 0;
 }
